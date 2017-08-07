655c6923be30f56f30276223c4f32bc5e5664dfc
Loren <lorenrose@icloud.com>
2017-08-06 20:42:17 -0500

    set up
    pseudo-code, html spine

---
+++ assets/javascript/game.js
@@ -0,0 +1,10 @@
+- start image
+
+- press any key to get started
+
+- set rules: 12 guesses, no repeat letters
+
+- read key, compare to correct letters. if correct fill in blank. if not correct, do not mark.
+	- take one guess away either way
+- if word guessed >>> mark win; show movie title and poster, play song; start next word
+- if word not guessed >>> show you lose!; start next word
