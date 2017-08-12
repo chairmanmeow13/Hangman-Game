33598f64e880dcc22bb9c2512e6a77e036d8f766
Loren <lorenrose@icloud.com>
2017-08-11 23:50:08 -0500

    final push before hw deadline
    the very little i could figure out how to do...

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,10 +1,45 @@
-- start image
+var remainingLives = 8;
+var wins = 0;
+var movies = {
+  m1: ["Star Wars"],
+  m2: ["The Godfather"],
+  m3: ["Silence of the Lambs"],
+  m4: ["West Side Story"],
+  m5: ["Braveheart"]
+};
 
-- press any key to get started
 
-- set rules: 12 guesses, no repeat letters
+//start game instructions
+document.onkeyup = function(event) {
+   var userGuess = event.key;
 
-- read key, compare to correct letters. if correct fill in blank. if not correct, do not mark.
-	- take one guess away either way
-- if word guessed >>> mark win; show movie title and poster, play song; start next word
-- if word not guessed >>> show you lose!; start next word
+   var currentWord = movies[Math.floor(Math.random() * movies.length)];
+   var str = 'currentWord'
+   var res = str.split("");
+   var wordLength = res.length;
+   var underscores = "";
+   for(i=0; i<wordLength; i++) {
+    underscores = underscores + "_ "
+   }
+
+// scoring mechanism:
+//    every keystroke is one less life
+//    if letter matches letter in array, fill in blank(s) with correct letter
+//    if key does not match, reveal no blanks
+//    if person runs out of lives, they lose
+//    if the person guesses all correct letters: 1) mark win 2) bring up next 
+//    word in array until there are no unguessed choices in array 3) 
+//    restart guess total 4) clear previously guessed letter box 5) start 
+//    playing soundtrack from correctly guessed movie
+
+var previousGuess = [];//array created from previously typed keys
+
+var html =
+          "<p>Wins: " + wins + "</p>" +
+          "<p>Current word: " + underscores + "</p>" +
+          "<p>Number of guesses remaining: " + remainingLives + "</p>" + 
+          "<p>Last guess: " + userGuess + "</p>" +
+          "<p>Letters already guessed: " + previousGuess + "</p>";
+        document.querySelector("#game").innerHTML = html;
+
+}
